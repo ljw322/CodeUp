6cc82d144751bd983c5e672197dda5527cf8d500
JIWON LEE <ljw322@hanmail.net>
2021-01-23 16:30:51 +0900

    greedy practice5 update

--- 1.그리디/practice5.py
+++ 1.그리디/practice5.py
@@ -1,34 +1,45 @@
 '''
 #05 볼링공 고르기 15:55 ~ 16:25 제한시간 1초
 두 사람이 볼링공을 고르는 경우의 수는? 시간초과 3초나옴.
+combination은 메모리 초과로 이어질 수 있다.
+경우의 수로 풀자. 각 무게별로 공이 몇개있는지 파악한다.
 '''
-from itertools import combinations
+# from itertools import combinations
 import time
-
 start_time = time.time()
 
 n, m = map(int, input().split())
-ball = list(map(int, input().split()))
+ballList = [0]*(n+1)
 
-balls = list(combinations(ball, 2))
+ball = list(map(int, input().split()))
+for _ in ball:
+    ballList[_] += 1
+
+untilcount = 0
+case = 0
+ball_weight = 1
+for _ in ballList:
+    if _ == 0: continue
+    untilcount += _
+    case += ball_weight * (n-untilcount)
+    print(case)
+    ball_weight += 1
+
+print(case)
+
+
+
+# balls = list(combinations(ball, 2))
 # print(balls)
 
-count = 0
-for x in balls:
-    if x[0] == x[1]: count+=1
+# count = 0
+# for x in balls:
+#     if x[0] == x[1]: count+=1
     
-# print(count)
-print(len(balls) - count)
+# # print(count)
+# print(len(balls) - count)
 
-# array = [0]*11
-# for x in ball:
-#     array[x] += 1
 
-# result = 0
-# for i in range(1, m+1):
-#     n-=array[i]
-#     result += array[i]*n
-# print(result)
 
 end_time = time.time()
 print("프로그램 수행시간: ", end_time - start_time)
